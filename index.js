18b6bc879f38f96398edb1600354786a47639421
TJ Horner <rowenrocks@gmail.com>
2014-09-27 20:06:02 -0700

    didn't test locally #yo #lo

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("FUCK");
 var app = require('express')();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -6,6 +5,8 @@ app.use(bodyParser.urlencoded());
 var httpServer = require('http').createServer(app).listen(process.env.PORT || 3000);
 var http = require('http');
 var io = require('socket.io').listen(httpServer);
+
+var sessions = {};
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
@@ -15,6 +16,7 @@ io.on('connection', function(socket){
 	socket.emit('hello', '');
 	socket.on('session:name', function(m){
 		socket.session = m;
+		socket.emit('soundcloud:song:change', (sessions[m] || ""));
 	});
 });
 
@@ -29,5 +31,6 @@ app.post('/song', function(req, res){
 			}
 		}
 	}
+	sessions[req.body.session] = req.body.url;
 	res.send(req.body.url);
 });
